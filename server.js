3567eefc3218087a077840c628230feae6fe5848
afdalprasetya6-byte <afdalprasetya6@gmail.com>
2026-06-06 15:08:37 +0700

    northflank deployment

--- server.js
+++ server.js
@@ -112,10 +112,13 @@ app.get(
 
 });
 
-app.listen(3000, () => {
+const PORT =
+    process.env.PORT || 3000;
+
+app.listen(PORT, () => {
 
     console.log(
-        "SERVER BERJALAN"
+        `SERVER BERJALAN DI PORT ${PORT}`
     );
 
 });
